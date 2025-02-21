9459bd1f591ef8ba161468b402e22c4337fdda65
dpma1122 <dpma1122@yahoo.com>
2025-02-21 10:46:11 -0500

    added key model pair for Schema

--- models/Journal.mjs
+++ models/Journal.mjs
@@ -2,4 +2,9 @@ import mongoose from 'mongoose';
 
 
 //build model
-const JournalSchema= new mongoose.Schema()
+const JournalSchema = new mongoose.Schema()
+
+//export to a key model pair
+const Journal = mongoose.model('Journal', JournalSchema)
+
+export default Journal; 
