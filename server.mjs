2cddd6218ecf200355bf0005ce5222f1c37a3494
dpma1122 <dpma1122@yahoo.com>
2025-02-21 09:49:05 -0500

    port is listening

--- server.mjs
+++ server.mjs
@@ -3,11 +3,13 @@ import express from 'express';
 import dotenv from 'dotenv'
 
 // setup
-
-
+const  app = express()
 //middleware
 
 
 //routes
 
 //listener
+app.listen(3000, () => {
+    console.log('Server Running on PORT:${3000}')
+})
