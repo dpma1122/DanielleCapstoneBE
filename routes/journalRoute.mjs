3d39d8a6e095d4cf8a655235451b0d0c8d915381
dpma1122 <dpma1122@yahoo.com>
2025-02-21 14:06:06 -0500

    added try catch for post request

--- routes/journalRoute.mjs
+++ routes/journalRoute.mjs
@@ -7,10 +7,67 @@ import express from 'express';
 
 
 //test a route
+// router.get('/', (req, res) => {
+//     res.send('Journal routes')
+// })
 
-router.get('/', (req, res) => {
-    res.send('Journal routes')
+//create
+router.post('/', (req, res) => {
+    try {
+
+    } catch (error) {
+            console.log(error); 
+            res.status(500).json({msg: 'Server Error'})
+        }
+    
 })
+
+
+
+
+
+//read
+router.get('/', (req, res) => {
+        try {
+        
+    } catch (error) {
+            console.log(error); 
+            res.status(500).json({msg: 'Server Error'})
+        }
+    
+})
+
+
+
+
+
+
+
+//update
+
+router.put('/', (req, res) => {
+    
+    try {
+        
+    } catch (error) {
+            console.log(error); 
+            res.status(500).json({msg: 'Server Error'})
+        }
+    
+})
+
+//delete
+router.delete('/', (req, res) => {
+    
+    try {
+        
+    } catch (error) {
+            console.log(error); 
+            res.status(500).json({msg: 'Server Error'})
+        }
+    
+})
+
 
 
 
