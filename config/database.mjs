7fdb8ad0de0d23b91abedcb80c96829eef1786a3
dpma1122 <dpma1122@yahoo.com>
2025-02-21 10:25:05 -0500

    added function to connectDB

--- config/database.mjs
+++ config/database.mjs
@@ -3,3 +3,11 @@ import mongoose from "mongoose";
 
 //connecting string variable
 const db = process.env.mongoURI
+
+  export default async function connectDB() {
+      try {
+      } catch (error) {
+          console.log(error) 
+          process.exit (1)
+        }
+    }
